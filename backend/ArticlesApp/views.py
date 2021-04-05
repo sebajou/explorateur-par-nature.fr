f4f7cabbe444c2ae0f15214d7104b563efe9aec6
sebajou <sebajou@gmail.com>
2021-04-05 15:12:42 +0200

    Set Tokens authentifiaction for API

--- backend/ArticlesApp/views.py
+++ backend/ArticlesApp/views.py
@@ -3,44 +3,53 @@ from rest_framework import viewsets
 from .serializers import ArticleSerializer, BibliographySerializer, EquipmentSerializer, ImageSerializer, \
     ListImageSerializer, ListEquipmentSerializer, ListVideoSerializer, VideoSerializer
 from .models import Article, Bibliography, Equipment, Image, ListImage, ListEquipment, ListVideo, Video
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 
 class ArticleView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
 
 
 class BibliographyView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = BibliographySerializer
     queryset = Bibliography.objects.all()
 
 
 class EquipmentView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = EquipmentSerializer
     queryset = Equipment.objects.all()
 
 
 class ImageView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = ImageSerializer
     queryset = Image.objects.all()
 
 
 class ListImageView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = ListImageSerializer
     queryset = ListImage.objects.all()
 
 
 class ListEquipmentView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = ListEquipmentSerializer
     queryset = ListEquipment.objects.all()
 
 
 class ListVideoView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = ListVideoSerializer
     queryset = ListVideo.objects.all()
 
 
 class VideoView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = VideoSerializer
     queryset = Video.objects.all()
--- backend/UsersApp/views.py
+++ backend/UsersApp/views.py
@@ -6,45 +6,41 @@ from .serializers import MyuserSerializer, BadgeSerializer, TributSerializer, Ch
     TutorLinkSerializer
 from .models import Myuser, Badge, Tribut, Child, Trophies, TutorLink
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 # Create your views here.
 
 
-class MyuserView(viewsets.ModelViewSet, APIView):
+class MyuserView(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
     serializer_class = MyuserSerializer
     queryset = Myuser.objects.all()
 
-    def get(self, request, format=None):
-        content = {
-            'user': str(request.user),  # `django.contrib.auth.User` instance.
-            'auth': str(request.auth),  # None
-        }
-        return Response(content)
-
 
 class BadgeView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = BadgeSerializer
     queryset = Badge.objects.all()
 
 
 class TributView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = TributSerializer
     queryset = Tribut.objects.all()
 
 
 class ChildView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = ChildSerializer
     queryset = Child.objects.all()
 
 
 class TrophiesView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = TrophiesSerializer
     queryset = Trophies.objects.all()
 
 
 class TutorLinkView(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = TutorLinkSerializer
     queryset = TutorLink.objects.all()
